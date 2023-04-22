70d448a931b480e9e3d50826b64c2cc427d3a888
Richa Jain <jain.ric@northeastern.edu>
2023-04-21 20:17:46 -0400

    Router for getAllUsers and deleteUser added

--- final-project-arcademania-main/server/src/routes/user/user-router.ts
+++ final-project-arcademania-main/server/src/routes/user/user-router.ts
@@ -8,7 +8,6 @@ const router = express.Router();
 // getUser Route
 router.route('/user/getUser').get(authRoute, userController.getUser);
 
-
 // UpdateProfile Route
 router.route('/user/updateProfile').post(authRoute, userController.updateProfile);
 
